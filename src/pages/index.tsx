4a46b4320ca0dab783904ac353fa304be6901221
Milena Kukolj <mkukolj@protonmail.com>
2023-02-23 13:41:53 +0100

    Initial commit.

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { type NextPage } from "next";
+const Home: NextPage = () => {
+
+  return (
+    <>
+      <h1 className="text-2xl text-red-200">Home</h1>
+    </>
+  )
+};
+
+export default Home;
